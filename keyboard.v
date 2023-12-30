ba47ab63241f79af4ee2986b43d2de8f1e03a316
_klein_ <_352641075@qq.com_>
2023-12-31 00:37:35 +0800

    kb

--- keyboard.v
+++ keyboard.v
@@ -2,49 +2,66 @@ module keyboard (
     input wire clk,
     input wire kb_clk,
     input wire data,
-    output reg [7:0] keycode
+    output reg [7:0] keycode,
+    output reg sign
 );
   reg [1:0] state = 0;  // 00 wait 01 input 10 confirm 11 conculde
   reg [7:0] code_temp = 0;
   reg [2:0] cnt = 0;
-  reg confirm=0;
-  always @(negedge kb_clk or posedge clk) begin
-    if (clk) begin
-      if(state==2'b11)begin
-        keycode <= code_temp;
-        state <= 2'b00;
-      end else begin
-        keycode <=0;
-      end
-    end else begin
-      case (state)
-        2'b00: begin
+  reg confirm = 0;
+  reg [2:0] kb_clk_old = 0;
+  initial begin
+    keycode = 8'h00;
+    sign = 0;
+  end
+  always @(posedge clk) begin
+    kb_clk_old <= {kb_clk_old[1:0], kb_clk};
+    case (state)
+      2'b00: begin
+        if (kb_clk_old[2] & ~kb_clk_old[1]) begin
           if (!data) begin
-            state <= 2'b01;
+            if ((&code_temp[7:4]) && ~(|code_temp[3:0])) begin
+              sign <= 0;
+            end else begin
+              sign <= 1;
+            end
+            state   <= 2'b01;
             confirm <= 0;
           end
         end
-        2'b01: begin
+        keycode <= 8'h00;
+      end
+      2'b01: begin
+        if (kb_clk_old[2] & ~kb_clk_old[1]) begin
           if (&cnt) begin
             state <= 2'b10;
             cnt   <= 0;
           end else begin
             cnt <= cnt + 1;
           end
-          if(data)begin
+          if (data) begin
             confirm <= ~confirm;
           end
           code_temp = {code_temp[6:0], data};
         end
-        2'b10: begin
-          if (confirm^data) begin
+        keycode <= 8'h00;
+      end
+      2'b10: begin
+        if (kb_clk_old[2] & ~kb_clk_old[1]) begin
+          if (~(confirm ^ data)) begin
             state <= 2'b11;
-          end
-          else begin
+          end else begin
             state <= 2'b00;
+            code_temp <= 8'h00;
           end
         end
-      endcase
-    end
+        keycode <= 8'h00;
+      end
+      2'b11:
+      begin
+        state <= 2'b00;
+        keycode <= code_temp;
+      end
+    endcase
   end
 endmodule
